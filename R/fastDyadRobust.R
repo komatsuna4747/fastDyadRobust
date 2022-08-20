4d8e7d51ca8424dfb0c5872260d8fcd0a0f49382
komatsuna4747 <shota.komatsu0427@gmail.com>
2022-08-20 23:14:14 +0900

    Edit roxygen

--- R/fastDyadRobust.R
+++ R/fastDyadRobust.R
@@ -1,3 +1,13 @@
+#' fastDyadRobust
+#'
+#' @description
+#' Computes dyad-robust standard errors via multiway decomposition,
+#' proposed by Aronow, Peter M., Cyrus Samii, and Valentina A. Assenova.
+#' "Cluster-robust variance estimation for dyadic data." Political Analysis 23.4 (2015): 564-577
+#' (\code{\href{https://arxiv.org/pdf/1312.3398.pdf}{ARXIV}}).
+#'
+#' This package is based on \code{dyadRobust}.
+#'
 #' @useDynLib fastDyadRobust
 #' @importFrom Rcpp sourceCpp
 #'
@@ -6,15 +16,6 @@
 #' The dimension of `dyad_cluster` must be D x 2,
 #' where D is the number of rows of the data frame that you have used to estimate `fit`.
 fastDyadRobust <- function(fit, dyad_cluster) {
-
-  # Check fit
-  if (class(fit) != "fixest") {
-    stop("fixestDyadRobust() only supports fixest objects.")
-  }
-
-  if (fit$method != "feols") {
-    stop("fixestDyadRobust() only supports feols models.")
-  }
 
   # Check dyad_cluster
   if (ncol(dyad_cluster) != 2) {
