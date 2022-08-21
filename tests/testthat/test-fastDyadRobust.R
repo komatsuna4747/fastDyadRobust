4e3721d06f4fa74f86837421d056da4ce21c5120
komatsuna4747 <shota.komatsu0427@gmail.com>
2022-08-21 16:30:56 +0900

    Add tests for errors

--- tests/testthat/test-fastDyadRobust.R
+++ tests/testthat/test-fastDyadRobust.R
@@ -69,7 +69,7 @@ df_test$y <- df_test$x + df_test$e
 # Compute dyad-robust standard errors using fastDyadRobust
 dyad_cluster <- df_test[c("Var1", "Var2")]
 reg_dyad <- lm(y ~ x, df_test)
-se_fastDyadRobust <- fastDyadRobust::fastDyadRobust(reg_dyad, dyad_cluster)
+se_fastDyadRobust <- fastDyadRobust(reg_dyad, dyad_cluster)
 
 # Compute dyad-robust standard errors using dyadRobust
 dyad_cluster_test <- dyad_cluster
@@ -79,4 +79,10 @@ se_dyadRobust <- testDyadRobust(reg_dyad, dyad_cluster_test)
 
 test_that("Correctly computing dyad-robust standard errors works", {
   expect_equal(se_fastDyadRobust, se_dyadRobust, tolerance = 1e-14)
+})
+
+# Check errors
+test_that("Error messages", {
+  expect_error(fastDyadRobust(reg_dyad, dyad_cluster[, 1, drop = FALSE]))
+  expect_error(fastDyadRobust(reg_dyad, dyad_cluster[1:10, ]))
 })
