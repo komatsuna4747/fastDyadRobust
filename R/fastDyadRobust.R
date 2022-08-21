0bc2e6a333b8275985616d2923a95fc2d2e02885
komatsuna4747 <shota.komatsu0427@gmail.com>
2022-08-21 10:37:06 +0900

    Edit fastDyadRobust()

--- R/fastDyadRobust.R
+++ R/fastDyadRobust.R
@@ -3,7 +3,8 @@
 #' @description
 #' Computes dyad-robust standard errors via multiway decomposition,
 #' proposed by Aronow, Peter M., Cyrus Samii, and Valentina A. Assenova.
-#' "Cluster-robust variance estimation for dyadic data." Political Analysis 23.4 (2015): 564-577.
+#' "Cluster-robust variance estimation for dyadic data."
+#' Political Analysis 23.4 (2015): 564-577.
 #'
 #' This package is based on \code{dyadRobust}.
 #'
@@ -11,9 +12,11 @@
 #' @importFrom Rcpp sourceCpp
 #'
 #' @param fit The model object estimated by the \code{fixest} package.
-#' @param dyad_cluster A data frame or matrix that specifies how to dyadically cluster the standard errors.
+#' @param dyad_cluster A data frame or matrix that specifies how to dyadically
+#' cluster the standard errors.
 #' The dimension of `dyad_cluster` must be D x 2,
-#' where D is the number of rows of the data frame that you have used to estimate `fit`.
+#' where D is the number of rows of the data frame that you have used
+#' to estimate `fit`.
 #'
 #' @export
 fastDyadRobust <- function(fit, dyad_cluster) {
@@ -23,21 +26,24 @@ fastDyadRobust <- function(fit, dyad_cluster) {
     stop("dyad_cluster must have just two columns.")
   }
 
-  # Extract ids for dyad-robust clustering
+  est_fun <- sandwich::estfun(fit)
+
+  if (nrow(dyad_cluster) != nrow(est_fun)) {
+    stop("The number of rows of dyad_cluster doesn't match the number of observations of fit.")
+  }
+
+  # Extract and format ids for dyad-robust clustering
   list_data <- clean_dyad_cluster(dyad_cluster)
 
   # Create dyad id
   dyad_id <-
     paste(list_data$dyad_cluster[, 1], list_data$dyad_cluster[, 2], sep = "-")
 
-  # Prepare
-  est_fun <- sandwich::estfun(fit)
-
   # Prepare meat and bread
   meat <- create_meat(est_fun, list_data$dyad_cluster, list_data$id)
   bread <- sandwich::bread(fit)
 
-  #
+  # Compute dyad-robust standard errors via multiway decomposition
   dyad_robust_vcov <-
     1/nrow(est_fun) * (bread %*% meat %*% bread) -
     sandwich::vcovCL(fit, type = "HC0", cluster = dyad_id, cadjust = FALSE) -
