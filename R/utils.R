2878d22e7a4ab6b66f47ed5512e6efa26c82ce38
komatsuna4747 <shota.komatsu0427@gmail.com>
2022-08-21 14:51:05 +0900

    Fix bugs in utils

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,6 @@
 clean_dyad_cluster <- function(dyad_cluster) {
   dyad_cluster <- as.matrix(dyad_cluster)
-  id <- unique(c(dyad_cluster[, 1], dyad_cluster[, 2]))
+  id <- sort(unique(c(dyad_cluster[, 1], dyad_cluster[, 2])))
 
   if (!is.integer(id)) {
     id <- as.factor(id)
@@ -11,8 +11,14 @@ clean_dyad_cluster <- function(dyad_cluster) {
     id <- as.integer(id)
   }
 
-  dyad_cluster[which(dyad_cluster[, 1] > dyad_cluster[, 2])] <-
-    rev(dyad_cluster[which(dyad_cluster[, 1] < dyad_cluster[, 2])])
+  # Conditionally swapping so that col1 <= col2.
+  i <- which(dyad_cluster[, 1] > dyad_cluster[, 2])
+
+  v1 <- dyad_cluster[i, 1]
+  v2 <- dyad_cluster[i, 2]
+
+  dyad_cluster[i, 1] <- v2
+  dyad_cluster[i, 2] <- v1
 
   return(list("id" = id, "dyad_cluster" = dyad_cluster))
 }
