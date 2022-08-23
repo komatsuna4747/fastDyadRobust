5f7e0c2849ed9d2fdb398bc1061531fc2fc6bb81
komatsuna4747 <shota.komatsu@sansan.com>
2022-08-23 11:01:05 +0900

    Add tests for feolsDyadRobust()

---
+++ tests/testthat/test-feolsDyadRobust.R
@@ -0,0 +1,20 @@
+set.seed(1878237564)
+
+df_test <- make_test_data(repeated = FALSE)
+
+# Run regression using feolsDyadRobust
+reg_feols <- feolsDyadRobust(y ~ x | Var1 + Var2, data = df_test, cluster = c("Var1", "Var2"))
+vcov(reg_feols)
+
+test_that("Check if diagonal elements of vcov are all positive", {
+  expect_true(diag(vcov(reg_feols)) > 0)
+})
+
+test_that("Error handling correctly works", {
+  # No cluster argument
+  expect_error(feolsDyadRobust(y ~ x | Var1 + Var2, data = df_test))
+
+  # Inappropriate cluster argument
+  expect_error(feolsDyadRobust(y ~ x | Var1 + Var2, data = df_test, cluster = ~Var1 + Var2))
+  expect_error(feolsDyadRobust(y ~ x | Var1 + Var2, data = df_test, cluster = "Var1"))
+})
